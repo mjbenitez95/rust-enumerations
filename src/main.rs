8e1e8d15dd99bd43466bdc55d38d30a94a76d0dd
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-27 15:59:09 -0800

    experiment with an enum with various different types inside it

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,27 @@ enum IpAddrKind{
     V6(String),
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit, 
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call (&self) {
+        println!("{:?}", self)
+    }
+}
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
     let home = IpAddrKind::V4(127, 0, 0, 1);
-    let loopback = IpAddrKind::V6(String::from("::1"))
+    let loopback = IpAddrKind::V6(String::from("::1"));
 
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
