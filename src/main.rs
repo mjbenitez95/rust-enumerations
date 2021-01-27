4000a7a2172cd58b91799467e21505bb6b421bc3
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-27 15:47:48 -0800

    implement v4 addresses as a series of u8 values

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 enum IpAddrKind{
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
-}
-
-fn routes(ip_kind: IpAddrKind) {
-
 }
 
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
-    let home = IpAddrKind::V4(String::from("127.0.0.1"))
+    let home = IpAddrKind::V4(127, 0, 0, 1);
     let loopback = IpAddrKind::V6(String::from("::1"))
 
 }
