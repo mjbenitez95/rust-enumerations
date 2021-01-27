fad339e6b6312cf6b3972423a228ea6375689499
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-27 15:40:28 -0800

    make more concise by attaching data to enum directly

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 enum IpAddrKind{
-    V4,
-    V6
-}
-
-struct IpAddr{
-    kind: IpAddrKind,
-    address: String,
+    V4(String),
+    V6(String),
 }
 
 fn routes(ip_kind: IpAddrKind) {
@@ -16,14 +11,7 @@ fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let home = IpAddrKind::V4(String::from("127.0.0.1"))
+    let loopback = IpAddrKind::V6(String::from("::1"))
 
 }
