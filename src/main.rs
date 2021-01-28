ec71072fb512b2a4ca5378f29d9bfec79af01e3d
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-27 16:04:50 -0800

    introduce the Option<T> enum

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-enum IpAddrKind{
+enum IpAddrKind {
     V4(u8, u8, u8, u8),
     V6(String),
 }
 
 #[derive(Debug)]
 enum Message {
-    Quit, 
+    Quit,
     Move { x: i32, y: i32 },
     Write(String),
     ChangeColor(i32, i32, i32),
 }
 
 impl Message {
-    fn call (&self) {
+    fn call(&self) {
         println!("{:?}", self)
     }
 }
@@ -26,4 +26,14 @@ fn main() {
 
     let m = Message::Write(String::from("hello"));
     m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("This is a string");
+
+    let absent_number: Option<i32> = None;
+
+    println!(
+        "Some number: {:?} and some string: \"{:?}\" and absent number == some: {}.",
+        some_number, some_string, absent_number == Some(5)
+    );
 }
